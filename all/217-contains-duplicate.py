7c2e330b1bdd329855457c136137fc5abfbf05c9
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2024-01-25 14:42:57 -0800

    question 49

--- all/217-contains-duplicate.py
+++ all/217-contains-duplicate.py
@@ -5,13 +5,13 @@
     Space Complexity: O(n)
 """
 
-def containsDuplicate(self, nums: [int]) -> bool:
+def containsDuplicate(self, nums) -> bool:
     # Set to keep track of what numbers are iterated
-    seen_numbers = []
+    seen_numbers = set()
     for num in nums:
         # If number is in the set, this is a duplicate
         if num in seen_numbers:
             return True
         seen_numbers.add(num)
 
-    return True
+    return False
---
+++ all/49-group-anagrams.py
@@ -0,0 +1,31 @@
+"""
+    Problem: https://leetcode.com/problems/group-anagrams/description
+    Approach: hashing the frequency of characters to a string or a tuple (in Python)
+    Time Complexity: O(nk), n is the length of the list and k is the length of a string
+    Space Complexity: O(nk)
+"""
+def groupAnagrams(self, strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+
+    def parse_word(word):
+        """
+        Convert a word to a tuple of letter frequency
+
+        Args:
+            word: str
+        """
+        freq = [0] * 26
+        for i in word:
+            freq[ord(i) - ord('a')] += 1
+        return tuple(freq)
+
+    # A dictionary with key as the parsing value and value as a list of anagrams
+    groups = dict()
+    for word in strs:
+        parse_value = parse_word(word)
+        groups.setdefault(parse_value, [])
+        groups[parse_value].append(word)
+    return groups.values()
