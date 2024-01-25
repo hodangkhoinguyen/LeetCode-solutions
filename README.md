858dbb0244f4366ab2aae6ef2bf545d811d23404
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2024-01-25 14:28:18 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode Solutions
+
+Here are all my LeetCode solutions so far. I will try to work on 1-2 new questions daily with 2 previously completed ones and post here.
+
+Most solutions are in Python and some in Java.
---
+++ all/217-contains-duplicate.py
@@ -0,0 +1,17 @@
+"""
+    Problem: https://leetcode.com/problems/contains-duplicate/description
+    Approach: hash set
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+"""
+
+def containsDuplicate(self, nums: [int]) -> bool:
+    # Set to keep track of what numbers are iterated
+    seen_numbers = []
+    for num in nums:
+        # If number is in the set, this is a duplicate
+        if num in seen_numbers:
+            return True
+        seen_numbers.add(num)
+
+    return True
